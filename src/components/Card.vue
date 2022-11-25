7501e1876f4a6173d9f64617ca5dd387d6e67537
adxell <adxell02@gmail.com>
2022-11-25 18:18:11 -0500

    create components main and card to show information about avatar

---
+++ src/components/Card.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="item">
+    <div class="image-contianer">
+      <slot name="image"></slot>
+    </div>
+    <div class="details">
+      <h3>
+        <slot name="heading"></slot>
+      </h3>
+      <span>
+        <slot name="origin"></slot>
+      </span>
+      <span>
+        <slot name="status"></slot>
+      </span>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.item {
+  display: flex;
+  flex-direction: column;
+  min-width: 100%;
+  padding: 10px 0px;
+}
+h3{
+  padding: 0px 10px;
+}
+span {
+  display: block;
+  font-size: 18px;
+  color: #fff;
+  padding: 0px 10px;
+}
+
+.item .image-contianer{
+  width: auto;
+  height: auto;
+  overflow: hidden;
+}
+
+
+.details {
+  background-color: rgb(99, 96, 96);
+}
+
+
+h3 {
+  font-size: 1.2rem;
+  font-weight: 500;
+  color: var(--color-heading);
+}
+
+@media (min-width: 768px) {
+  .item{
+    padding: 10px;
+  }
+}
+@media (min-width: 1024px) {}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import Card from './Card.vue'
+import type { Result } from '../interfaces/avatar.interface'
+
+defineProps<{
+  informationAboutAvatar: Result[],
+}>()
+</script>
+
+<template>
+  <div class="container_mean">
+  <template v-for="avatar in informationAboutAvatar">
+      <Card>
+        <template #image><img class="image_post" :src="avatar.image" :alt="avatar.name"></template>
+        <template #heading>{{ avatar.name}}</template>
+        <template #origin>Origin: {{avatar.origin.name}}</template>
+        <span><strong>Status:</strong> {{avatar.status}}</span>
+        <span><strong>Gender:</strong> {{avatar.gender}}</span>
+        <span><strong>Location:</strong> {{avatar.location.name}}</span>
+      </Card>
+    </template>
+  </div>
+</template>
+
+<style scoped>
+.container_mean{
+  display: grid;
+  grid-template-columns: 1fr;
+}
+span {
+  display: block;
+  font-size: 18px;
+  color: #fff;
+  padding: 0px 10px;
+}
+
+strong{
+  font-weight: 600;
+}
+.image_post {
+  flex-shrink: 0;
+  min-width: 100%;
+  min-height: 100%;
+  object-fit: cover;
+  transition: all 0.5s ease-in-out;
+}
+.image_post:hover {
+  transform: scale(1.2);
+}
+
+
+@media (min-width: 768px) {
+  .container_mean{
+    grid-template-columns: 1fr 1fr;
+  }
+}
+
+@media (min-width: 1024px) {
+  .container_mean{
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+}
+</style>
