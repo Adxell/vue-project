9a283aff9be1578d43f18384b55d67999413725c
adxell <adxell02@gmail.com>
2022-11-25 18:22:29 -0500

    delete component that was without use

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { onMounted, ref } from "vue"
+import axios from 'axios'
+import Main from './components/Main.vue'
+const informationAboutAvatar = ref([])
+
+async function getData() {
+  const { data } = await axios.get('https://rickandmortyapi.com/api/character')
+  informationAboutAvatar.value = data.results
+}
+
+onMounted(async()=> {
+  await getData()
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <Main :informationAboutAvatar="informationAboutAvatar" />
   </main>
 </template>
 
 <style scoped>
+
 header {
   line-height: 1.5;
 }
@@ -28,20 +32,13 @@ header {
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
 
   .logo {
     margin: 0 2rem 0 0;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  main {
+    width: 100%;
   }
 }
 </style>
